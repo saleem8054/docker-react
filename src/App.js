cbfa9a88e00352233d1575f9b5c1b6470a19ddf9
root <root@localhost.localdomain>
2020-10-29 18:36:17 +0100

    Removed Builder

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Everyone
+          Hello Everyone to my first ever React App
         </p>
         <a
           className="App-link"
