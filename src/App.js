a8240377ddda65e8448c35808263b6fcd8dd3012
root <root@localhost.localdomain>
2020-10-29 16:00:45 +0100

    Was to am

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed at the feature branch
+          I am changed at the feature branch
         </p>
         <a
           className="App-link"
