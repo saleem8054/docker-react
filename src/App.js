e1c8b4be8f4650aa6d4cf34f40e140daa959520f
root <root@localhost.localdomain>
2020-10-29 15:24:01 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to my little cute world.
+          I was changed at the feature branch
         </p>
         <a
           className="App-link"
